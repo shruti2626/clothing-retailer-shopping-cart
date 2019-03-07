aea73fb2a350bff9dfdf5f01591e696b9cd2c33c
shruti26 <47973195+shruti2626@users.noreply.github.com>
2019-03-07 12:16:32 +0000

    Update ProductDetailService_test.js

--- tests/services/ProductDetailService_test.js
+++ tests/services/ProductDetailService_test.js
@@ -1,1 +1,26 @@
 
+"use strict";
+describe("eCommerce.ProductDetailService_test", function () {
+
+    beforeEach(module("ecommerceApp"));
+
+    var service, output, $httpBackend
+    beforeEach(inject(function ($injector, _$httpBackend_) {
+        $httpBackend = _$httpBackend_;
+        $httpBackend.whenGET('views/products.html').respond('');
+        service = $injector.get("ProductDetailService");
+    }));
+
+    describe("test getProduct", function () {
+        it("test with response data", function () {
+            inject(function ($http) {
+                spyOn($http, "get").and.returnValue("output");
+               output = service.getProducts();
+                expect($http.get).toHaveBeenCalledWith(reqData);
+                expect(output).toBe("output");
+            });
+        });
+        
+    });
+    
+});
